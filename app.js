a187611910b97ed473484990e382f3c8a5e00f1c
Erik Longuepee <ec1991@gmail.com>
2021-01-03 23:51:44 +0100

    added basic version of subnetting CIDR calculator

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
+
+const subnetting = require('./subnetting.js');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
 require('dotenv').config({path: __dirname + '/.env'})
 
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Serve app homepage
 app.get('/', (req, res)=> {
     res.send("Welcome to the homepage BAY-BEE");
 })
 
+app.post('/api/subnetting/findOnceCIDR', (req,res) => {
+    let numHosts = parseInt(req.body.numHosts);
+    let networkBits = req.body.networkBits;
+    
+    let CIDRNotation = subnetting.findCIDRNotation(numHosts, networkBits);
+    console.log(`Finding CIDR for one subnet with ${numHosts} hosts and ${networkBits} network bits` );
+    res.json({
+        CIDRNotation: CIDRNotation
+    })
+});
+
 // A microservice for subnetting test
-app.get('/subnet/microservice', (req, res)=> {
+app.get('/api/subnetting', (req, res)=> {
     res.json({
         response: "Hello this is a subnetting microservice"
     });
