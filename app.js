aa22cecef4b18c3d303219a0b2bdb865aa2d7bb2
Erik Longuepee <ec1991@gmail.com>
2021-01-03 13:39:21 +0100

    adding an api test

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ app.get('/', (req, res)=> {
     res.send("Welcome to the homepage BAY-BEE");
 })
 
+// A microservice for subnetting test
+app.get('/subnet/microservice', (req, res)=> {
+    res.json({
+        response: "Hello this is a subnetting microservice"
+    });
+})
+
 app.listen(process.env.PORT, () => {
     console.log("Server is listening on port " + process.env.PORT);
 });
+
 
