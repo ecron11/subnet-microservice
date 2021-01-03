84036a537ace817a2fda7c32a104e709a8ec3ced
Erik Longuepee <ec1991@gmail.com>
2021-01-03 12:55:28 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+require('dotenv').config({path: __dirname + '/.env'})
+
+app.get('/', (req, res)=> {
+    res.send("Welcome to the homepage");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server is listening on port " + process.env.PORT);
+});
+
